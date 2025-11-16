575373e127048d2c410e4f9060b49d51f096ec16
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2025-11-15 17:06:18 -0700

    fixed the horizontal calendar scrolling behavior

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -33,20 +33,15 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
 
   return (
     <div className="flex min-h-screen flex-col">
-      {/* <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="p-4 flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
-        </div>
-      </header> */}
       <div className="grid flex-1 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[225px] flex-col md:flex h-[calc(100vh-4rem)]">
+        <aside className="hidden h-full w-[225px] flex-col md:flex">
           <DashboardNav
             items={dashboardConfig.sidebarNav}
             teams={teams}
             currentTeamId={currentTeamId}
           />
         </aside>
-        <main className="w-full flex flex-1 flex-col overflow-y-auto h-[calc(100vh-4rem)] px-8">
+        <main className="h-full flex flex-1 flex-col overflow-y-auto px-8">
           {children}
         </main>
       </div>
